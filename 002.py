b094e47a7c729b669b19efd3d9005d4c80d5cfa9
GustavoJatene <jateneg@gmail.com>
2020-05-27 12:18:15 -0300

    Operações matemáticas

---
+++ 002.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite o primeiro valor: "))
+n2 = int(input("Digite o segundo valor: "))
+# também se pode criar variáveis para receber os valores das operações
+print("Esse é o resultado da SOMA: {}".format(n1+n2))
+print("Esse é o resultado da SUBTRAÇÃO: {}".format(n1-n2))
+print("Esse é o resultado da MULTIPLICAÇÃO: {}".format(n1*n2))
+print("Esse é o resultado da DIVISÃO: {}".format(n1/n2))
