4ed579c3808444c4b5bf36dd2aecf52627a94f18
GustavoJatene <jateneg@gmail.com>
2020-05-28 11:43:32 -0300

    Usando objetos string

---
+++ 009.py
@@ -0,0 +1,11 @@
+print("Python é uma maldição!")
+var1 = "Python é uma maldição!"
+print("O primeiro caracter da frase é: {}".format(var1[0]))
+print("Do primeiro até o sexto carater é: {}".format(var1[:6]))
+print(("O total de caracteres é: {}".format(len(var1))))
+print("dados é o novo petróleo, em Deus eu confio, para o resto me traga dados..")
+var2 = "dados é o novo petróleo, em Deus eu confio, para o resto me traga dados.."
+print("dados aparece {} vezes".format(var2.count("dados")))
+print("Usando a função \'capitalize\': {}".format(var2.capitalize()))
+print("Usando a função \'split\': {}".format(var2.split()))
+
