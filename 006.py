1757812ac4ddc39311f1906799ec94640f69c9cd
GustavoJatene <jateneg@gmail.com>
2020-05-27 23:32:14 -0300

    Usando a função round()

---
+++ 006.py
@@ -0,0 +1,5 @@
+
+r = float(input("Digite um numero real: "))
+print("O valor arredondado é: {:.1f}".format(round(r)))
+n = float(input("Digite outro n real com mais de 4 casas após a virgula: "))
+print("O valor arredondado com 4 casas decimais é: {}".format(round(n,4)))
