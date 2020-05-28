06be3b40d4f0216dbee74354a1076cc9cc5119fa
GustavoJatene <jateneg@gmail.com>
2020-05-28 11:11:26 -0300

    Explicando erro de uma instrução

---
+++ 008.py
@@ -0,0 +1,6 @@
+# 8) Explique a causa do erro se executar a seguinte instrução para criar uma nova variável: break=10
+break = 10
+'''
+Pois estamos atribuindo um valor a uma palavra reservada do sistema.
+Assim como não podemos atribuir valores ao (for, while, if...)
+'''
